59455a8a0f7a5d1564789185c4a56ca484d79dd9
brucesz <zsyuyizhang@gmail.com>
2020-06-15 10:31:05 +0800

    add hw1

---
+++ hw_1/workspace/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/brucesz/Desktop/pnc_learn/hw_1/cw/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/opt/ros/melodic".split(';') if "/opt/ros/melodic" != "" else []
---
+++ hw_1/workspace/build/waypoint_generator/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/brucesz/Desktop/pnc_learn/hw_1/cw/src/waypoint_generator
+# Build directory: /home/brucesz/Desktop/pnc_learn/hw_1/cw/build/waypoint_generator
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
