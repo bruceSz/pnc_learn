d851b2c24b6466e9a08f1dc60935791764d2767c
brucesz <zsyuyizhang@gmail.com>
2020-06-17 09:32:17 +0800

    remove build and deve from hw_1 dir

--- hw_1/workspace/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/brucesz/Desktop/pnc_learn/hw_1/cw/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- hw_1/workspace/build/rviz_plugins/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brucesz/Desktop/pnc_learn/hw_1/cw/src/rviz_plugins
-# Build directory: /home/brucesz/Desktop/pnc_learn/hw_1/cw/build/rviz_plugins
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
